b4f7a9624db7b600b0567aa461938a1ba2a22014
GauravN <f20241056@pilani.bits-pilani.ac.in>
2026-08-29 18:17:35 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -15,11 +15,11 @@ module FA_Gate(
   output cout
 );
   wire ps, pc1, pc2;
-and #(2) (pc1, a,   b);
-and #(2) (pc2, cin, ps);
-xor #(2) (sum, cin, ps);
-xor #(2) (ps,  a,   b);
-or  #(2) (cout, pc1, pc2);
+and  (pc1, a,   b);
+and  (pc2, cin, ps);
+xor (sum, cin, ps);
+xor  (ps,  a,   b);
+or   (cout, pc1, pc2);
 
 
 endmodule
